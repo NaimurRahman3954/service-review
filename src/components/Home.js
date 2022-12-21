1d2d95af09456414c107a9953bde366e4a63f9d6
NaimurRahman3954 <mnrahaman3954@gmail.com>
2022-12-22 00:21:16 +0600

    banner updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import AboutMe from './AboutMe'
 import Banner from './Banner'
 import Contact from './Contact'
+import Pricing from './Pricing'
 import Services from './Services'
 import Stats from './Stats'
 
@@ -10,12 +11,16 @@ const Home = () => {
   return (
     <div className="App">
       <Banner></Banner>
-      <h1 className="text-5xl font-bold mt-12">Services I offer</h1>
+      <h1 className="text-5xl font-bold mt-32">Services I offer</h1>
       <Services></Services>
       <Link to="/services">
         <button className="btn btn-primary mb-6">See All →</button>
       </Link>
       <AboutMe></AboutMe>
+      <h1 className="text-5xl font-bold mt-32">
+        Become a <span className="text-primary">Pro</span> Member
+      </h1>
+      <Pricing></Pricing>
       <Stats></Stats>
       <Contact></Contact>
     </div>
