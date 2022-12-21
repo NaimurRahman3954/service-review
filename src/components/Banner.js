3a23adb1a202edd143706af03038c0bbc35b32b6
NaimurRahman3954 <mnrahaman3954@gmail.com>
2022-12-21 23:50:06 +0600

    banner updated

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -45,7 +45,24 @@ const Banner = () => {
           </div>
         </div>
         <div id="item3" className="carousel-item w-full">
-          <img src="https://placeimg.com/800/200/arch" className="w-full" />
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <img
+              src="https://i.ibb.co/rx9BcwP/3-1.png"
+              className=" max-w-sm mr-16"
+              alt=""
+            />
+            <div className="text-left ml-24">
+              <h1 className="text-5xl font-bold">
+                IELTS Listening Preparation Mentorship Program
+              </h1>
+              <p className="py-6">
+                I will guide you for your IELTS listening preparation and help
+                you achieve the best score that will eventually lead you to a
+                successful career.
+              </p>
+              <button className="btn btn-primary">Learn More</button>
+            </div>
+          </div>
         </div>
         <div id="item4" className="carousel-item w-full">
           <img src="https://placeimg.com/800/200/arch" className="w-full" />
