d485f23a1c77cef462af4480b1617eaa50ad7963
NaimurRahman3954 <mnrahaman3954@gmail.com>
2022-12-21 23:53:58 +0600

    banner updated

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -31,7 +31,7 @@ const Banner = () => {
               className=" max-w-sm mr-16"
               alt=""
             />
-            <div className="text-left ml-24">
+            <div className="text-left px-9 lg:pl-24">
               <h1 className="text-5xl font-bold">
                 Complete IELTS Preparation Mentorship Program
               </h1>
@@ -51,7 +51,7 @@ const Banner = () => {
               className=" max-w-sm mr-16"
               alt=""
             />
-            <div className="text-left ml-24">
+            <div className="text-left px-9 lg:pl-24">
               <h1 className="text-5xl font-bold">
                 IELTS Listening Preparation Mentorship Program
               </h1>
@@ -71,7 +71,7 @@ const Banner = () => {
               className=" max-w-sm mr-16"
               alt=""
             />
-            <div className="text-left ml-24">
+            <div className="text-left px-9 lg:pl-24">
               <h1 className="text-5xl font-bold">
                 IELTS Reading Preparation Mentorship Program
               </h1>
@@ -85,7 +85,7 @@ const Banner = () => {
           </div>
         </div>
       </div>
-      <div className="flex justify-center w-full py-2 gap-2">
+      <div className="flex justify-center w-full pb-2 gap-2">
         <a href="#item1" className="btn btn-xs btn-outline">
           1
         </a>
@@ -98,122 +98,6 @@ const Banner = () => {
         <a href="#item4" className="btn btn-xs btn-outline">
           4
         </a>
-      </div>
-      {/* ----------- */}
-      <div className="carousel w-full border-1">
-        <div id="slide1" className="carousel-item relative w-full">
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            <img
-              src="https://i.ibb.co/q0p5DQt/1-1.png"
-              className=" max-w-sm lg:mr-16"
-              alt=""
-            />
-            <div className="text-left px-9 lg:pl-24">
-              <h1 className="text-5xl font-bold">
-                Computer-delivered IELTS Mock Test
-              </h1>
-              <p className="py-6">
-                I have developed country's first computer-delivered mock test
-                exactly like British Council so that you can be ready and feel
-                comfortable in your final text.
-              </p>
-              <button className="btn btn-primary">Learn More</button>
-            </div>
-          </div>
-
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide4" className="btn btn-circle btn-outline">
-              ❮
-            </a>
-            <a href="#slide2" className="btn btn-circle btn-outline">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide2" className="carousel-item relative w-full">
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            <img
-              src="https://i.ibb.co/w6Fr2VX/2-1.png"
-              className=" max-w-sm mr-16"
-              alt=""
-            />
-            <div className="text-left ml-24">
-              <h1 className="text-5xl font-bold">
-                Complete IELTS Preparation Mentorship Program
-              </h1>
-              <p className="py-6">
-                I will guide you throughout your IELTS journey and help you
-                achieve the best score that will eventually lead you to a
-                successful career.
-              </p>
-              <button className="btn btn-primary">Learn More</button>
-            </div>
-          </div>
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide1" className="btn btn-circle btn-outline">
-              ❮
-            </a>
-            <a href="#slide3" className="btn btn-circle btn-outline">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide3" className="carousel-item relative w-full">
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            <img
-              src="https://i.ibb.co/rx9BcwP/3-1.png"
-              className=" max-w-sm mr-16"
-              alt=""
-            />
-            <div className="text-left ml-24">
-              <h1 className="text-5xl font-bold">
-                IELTS Listening Preparation Mentorship Program
-              </h1>
-              <p className="py-6">
-                I will guide you for your IELTS listening preparation and help
-                you achieve the best score that will eventually lead you to a
-                successful career.
-              </p>
-              <button className="btn btn-primary">Learn More</button>
-            </div>
-          </div>
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide2" className="btn btn-circle btn-outline">
-              ❮
-            </a>
-            <a href="#slide4" className="btn btn-circle btn-outline">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide4" className="carousel-item relative w-full">
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            <img
-              src="https://i.ibb.co/7yDf68Y/4-1.png"
-              className=" max-w-sm mr-16"
-              alt=""
-            />
-            <div className="text-left ml-24">
-              <h1 className="text-5xl font-bold">
-                IELTS Reading Preparation Mentorship Program
-              </h1>
-              <p className="py-6">
-                I will guide you for your IELTS reading preparation and help you
-                achieve the best score that will eventually lead you to a
-                successful career.
-              </p>
-              <button className="btn btn-primary">Learn More</button>
-            </div>
-          </div>
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide3" className="btn btn-circle btn-outline">
-              ❮
-            </a>
-            <a href="#slide1" className="btn btn-circle btn-outline">
-              ❯
-            </a>
-          </div>
-        </div>
       </div>
     </div>
   )
